0677e93d5782e0ed088bc736cb2e6e541fec7c6a
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-04-09 21:14:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  const d1 = new Date(date1);
+  const d2 = new Date(date2);
+
+  const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
+  const utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
+
+  const msPerDay = 24 * 60 * 60 * 1000;
+  return (utc2 - utc1) / msPerDay;
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+const dateOne = prompt("Enter Start Date.");   // e.g., "2022-03-01"
+const dateTwo = prompt("Enter End Date.");     // e.g., "2022-03-15"
+alert(dateDiffInDays(dateOne, dateTwo));       // e.g., 14
+
